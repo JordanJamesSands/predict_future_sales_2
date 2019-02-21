0a04d9bf22725045cf7c62429d5f4b6de5aedaa1
JordanJamesSands <jordan.james.sands@gmail.com>
2019-02-21 16:26:23 +1100

    ideas.txt

--- eda/scripts/probe2.R
+++ eda/scripts/probe2.R
@@ -1,5 +1,7 @@
 #submitted the following constant solutions and got results
 
-subs <- c(0.2,0.25,0.3)
-public_score <- c()
+subs <- c(0.2,0.25,0.3,0.35,0.4)
+public_score <- c( 1.22032,1.21791, 1.21754,1.21922,1.22295)
+
+plot(subs,public_score)
 
