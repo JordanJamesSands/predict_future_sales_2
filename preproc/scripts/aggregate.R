ee2ac1229f3e811fc391d59bc7c64b2473edf2b2
JordanJamesSands <jordan.james.sands@gmail.com>
2019-02-26 16:19:18 +1100

    large commit, finished pipeline skeleton, need to commit more frequently

--- preproc/scripts/aggregate.R
+++ preproc/scripts/aggregate.R
@@ -1,8 +1,12 @@
 #a <- group_by(train1,date_block_num,shop_id,item_id) %>% summarise(sales = sum(item_cnt_day))
 #group all data other than item_cnt_day and item_price
-train2 <- group_by(train1,date_block_num,shop_id,item_id,item_category_id,shop_name,item_name,item_category_name) %>% summarise(sales = sum(item_cnt_day))
-train2$sales <- ifelse(train2$sales<0,0,train2$sales)
-train2$sales <- ifelse(train2$sales>20,20,train2$sales)
+
+library(dplyr)
+train1 <- read.csv('gen_data/train1.csv')
+
+train2 <- group_by(train1,date_block_num,shop_id,item_id,item_category_id,shop_name,item_name,item_category_name) %>% summarise(item_cnt_month = sum(item_cnt_day))
+train2$item_cnt_month <- ifelse(train2$item_cnt_month<0,0,train2$item_cnt_month)
+train2$item_cnt_month <- ifelse(train2$item_cnt_month>20,20,train2$item_cnt_month)
 
 write.csv(train2,'gen_data/train2.csv',row.names=FALSE)
 
