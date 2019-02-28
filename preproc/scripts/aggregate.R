9a61915182b5cf046fc254e5298fc30f08df17fb
JordanJamesSands <jordan.james.sands@gmail.com>
2019-02-28 14:03:07 +1100

    separated clipping of processed data in to a separate script, clip.R, formerly part of aggregate.R

--- preproc/scripts/aggregate.R
+++ preproc/scripts/aggregate.R
@@ -4,11 +4,10 @@
 library(dplyr)
 train1 <- read.csv('gen_data/train1.csv')
 
-train2 <- group_by(train1,date_block_num,shop_id,item_id,item_category_id,shop_name,item_name,item_category_name) %>% summarise(item_cnt_month = sum(item_cnt_day))
-train2$item_cnt_month <- ifelse(train2$item_cnt_month<0,0,train2$item_cnt_month)
-train2$item_cnt_month <- ifelse(train2$item_cnt_month>20,20,train2$item_cnt_month)
+train2.1 <- group_by(train1,date_block_num,shop_id,item_id,item_category_id,shop_name,item_name,item_category_name) %>% summarise(item_cnt_month = sum(item_cnt_day))
 
-write.csv(train2,'gen_data/train2.csv',row.names=FALSE)
+
+write.csv(train2.1,'gen_data/train2.1.csv',row.names=FALSE)
 
 
 
