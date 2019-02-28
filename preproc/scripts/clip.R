a997bd07cf7f7ab7ea7a204f235dfd7ea9637c5c
JordanJamesSands <jordan.james.sands@gmail.com>
2019-02-28 15:52:01 +1100

    rechecked some of the pipeline data

--- preproc/scripts/clip.R
+++ preproc/scripts/clip.R
@@ -6,4 +6,4 @@ train2 <- train2.1
 train2$item_cnt_month <- ifelse(train2$item_cnt_month<0,0,train2$item_cnt_month)
 train2$item_cnt_month <- ifelse(train2$item_cnt_month>20,20,train2$item_cnt_month)
 
-write.csv(train2,'gen_data/train2.csv')
+write.csv(train2,'gen_data/train2.csv',row.names = FALSE)
